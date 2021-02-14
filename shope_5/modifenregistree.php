c77bb333c45152105557cf1aed5992a06153c861
Ikrame1307 <76827138+Ikrame1307@users.noreply.github.com>
2021-02-14 16:44:42 +0100

    Update modifenregistree.php

--- shope_5/modifenregistree.php
+++ shope_5/modifenregistree.php
@@ -5,12 +5,12 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel='stylesheet' href='profil.css'>
-    <title>Modifications enregisterées</title>
+    <title>Modifications enregistrées</title>
   </head>
 
   <body >
-  <div class="modification_enregistrée">
-      <h2>Modifications enregisterées</h2>
+  <div class="modification_enrgistrée">
+      <h2>Modifications enregistrées</h2>
       <a type="button" class="btn btn-success" href="profil.php">OK</a>
   </div>
     
